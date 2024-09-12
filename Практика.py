b17b234b26c90f465afed1ab0ab2761d11d5f052
ColD2201 <lexa.mayorov.02@mail.ru>
2024-09-13 02:38:15 +0300

    Add files via upload

---
+++ Практика.py
@@ -0,0 +1,13 @@
+﻿import datetime
+day = int(input("День: "))
+mouth = int(input("Месяц: "))
+year = int(input("Год: "))
+x = datetime.datetime(year,mouth,day)
+if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    print('Год високосный.')
+else:
+    print('Год не високосный.')
+print(x.strftime("%a"))
+real_date = datetime.datetime.now().year
+print("Ваш возраст:", str(real_date - year))
+print("Ваша полная дата рождения: ",str(day),".",str(mouth),".",str(year), sep="")
